1d947f7c3808dad3dd74996d2aac6a401c1e37d8
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2025-08-06 18:06:35 +0000

    Server side rendering

---
+++ dashboard/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+from django.conf import settings
+
+def home(request):
+    return render(request, 'dashboard/base.html')
+def index(request):
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+    total_responses = len(posts)
+    data = {
+        'title': "Landing Page' Dashboard",
+        'total_responses': total_responses,
+    }
+    return render(request, 'dashboard/index.html', data)
