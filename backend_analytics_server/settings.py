853e5c9f851dc94615f16633b481485c3d5387c8
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2025-08-13 19:33:47 +0000

    autenticacion

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -127,4 +127,19 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-API_URL = 'https://jsonplaceholder.typicode.com/posts'
+API_URL = 'http://danielz.pythonanywhere.com/landing/api/index/?format=json'
+
+CSRF_TRUSTED_ORIGINS = [
+  "https://*.app.github.dev", # Solo si utiliza Codespaces
+  "https://localhost:8000",
+  "http://127.0.0.1:8000"
+]
+ALLOWED_HOSTS = [
+  "*",
+]
+
+# Fallo: acceso sin autenticación
+LOGIN_URL = '/login/'
+
+# Éxito: luego de autenticación exitosa
+LOGIN_REDIRECT_URL = '/'
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
+"""
 def home(request):
     return render(request, 'dashboard/base.html')
+"""
+@login_required
 def index(request):
     response = requests.get(settings.API_URL)  # URL de la API
     posts = response.json()  # Convertir la respuesta a JSON
